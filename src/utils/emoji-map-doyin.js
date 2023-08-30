51d4464ffa67d32a5fec831f9432be56b375501e
黄泳康 <2607881950@qq.com>
2023-08-30 17:43:32 +0800

    抖音表情包

---
+++ src/utils/emoji-map-doyin.js
@@ -0,0 +1,42 @@
+// https://www.emojiall.com/images/60/douyin/clv.png
+export const emojiUrl = "https://www.emojiall.com/images/60/douyin/";
+export const localemojiUrl = "@/assets/emoji_doyin/";
+
+export const emojiMap = {
+  "[V5]": "clv.png",
+  "[给力]": "clw.png",
+  "[嘿哈]": "cm8.png",
+  "[加好友]": "cm9.png",
+  "[勾引]": "cmt.png",
+  "[机智]": "cn0.png",
+  "[来看我]": "cn1.png",
+  "[灵机一动]": "cn2.png",
+  "[困]": "cna.png",
+  "[疑问]": "cnb.png",
+  "[泣不成声]": "cnc.png",
+  "[小鼓掌]": "cnd.png",
+  "[发呆]": "cnf.png",
+  "[吐血]": "cnj.png",
+  "[酷拽]": "cnq.png",
+  "[泪奔]": "cnv.png",
+  "[抠鼻]": "co1.png",
+  "[互粉]": "co3.png",
+  "[去污粉]": "co8.png",
+  "[666]": "co9.png",
+  "[舔屏]": "cof.png",
+  "[鄙视]": "cog.png",
+  "[紫薇别走]": "coj.png",
+  "[不失礼貌的微笑]": "cop.png",
+  "[吐舌]": "coq.png",
+  "[呆无辜]": "cor.png",
+  "[白眼]": "cot.png",
+  "[吃瓜群众]": "cox.png",
+  "[绿帽子]": "coz.png",
+  "[皱眉]": "cp2.png",
+  "[擦汗]": "cp3.png",
+  "[强]": "cp7.png",
+  "[如花]": "cp8.png",
+  "[奋斗]": "cpc.png",
+};
+
+export const emojiName = [];
