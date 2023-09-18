655373c208f59fcf66b90a44335cd9fecb82a5c8
临江仙 <2607881950@qq.com>
2023-09-18 15:33:26 +0800

    test

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,10 @@
 import { createRouter, createWebHistory, createWebHashHistory, START_LOCATION } from "vue-router";
-import storage from "storejs";
-import NProgress from "@/utils/progress";
-import routes from "./routes";
-import store from "@/store";
 import { ACCESS_TOKEN } from "@/store/mutation-types";
-const { title, production } = require("@/config/vue.custom.config");
 import { setPageTitle } from "@/utils/common";
-import { getCookies } from "@/utils/Cookies";
+import NProgress from "@/utils/progress";
+import storage from "storejs";
+import routes from "./routes";
+const { title, production } = require("@/config/vue.custom.config");
 
 // hack router push callback
 const originalPush = createRouter.prototype.push;
@@ -27,19 +25,19 @@ console.log(routes);
 const router = createRouter({
   history: production ? createWebHashHistory() : createWebHistory(),
   routes,
-  strict: true,
-  scrollBehavior(to, from, savedPosition) {
-    return new Promise((resolve) => {
-      if (savedPosition) {
-        return savedPosition;
-      } else {
-        if (from.meta.saveSrollTop) {
-          const top = document.documentElement.scrollTop || document.body.scrollTop;
-          resolve({ left: 0, top });
-        }
-      }
-    });
-  },
+  // strict: true,
+  // scrollBehavior(to, from, savedPosition) {
+  //   return new Promise((resolve) => {
+  //     if (savedPosition) {
+  //       return savedPosition;
+  //     } else {
+  //       if (from.meta.saveSrollTop) {
+  //         const top = document.documentElement.scrollTop || document.body.scrollTop;
+  //         resolve({ left: 0, top });
+  //       }
+  //     }
+  //   });
+  // },
 });
 // 默认是  Hash  模式, 手动设置为  History  模式
 // 更新视图但不重新请求页面是前端路由原理的核心之一
@@ -49,9 +47,7 @@ router.beforeEach(async (to, from, next) => {
   if (from.path === to.path) return;
   setPageTitle(to.meta.title);
   const token = storage.get(ACCESS_TOKEN);
-  const tokenCopy = getCookies(ACCESS_TOKEN);
-
-  if (token && tokenCopy) {
+  if (token) {
     // start progress bar
     NProgress.start();
     if (isF) {
