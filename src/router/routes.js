b76f5403df37e6da2e375e0dad2493c93bd9bd7c
临江仙 <2607881950@qq.com>
2024-01-19 17:07:21 +0800

    routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -9,6 +9,27 @@ const routes = [
       icon: "Eleme",
     },
   },
+  // {
+  //   path: "/",
+  //   name: "home",
+  //   component: () => import(/* webpackChunkName: "Home" */ "@/views/home/index"),
+  //   meta: {
+  //     title: "home",
+  //     icon: "Eleme",
+  //   },
+  //   children: [
+  //     {
+  //       path: "/chatStudio",
+  //       name: "chatStudio",
+  //       component: () => import(/* webpackChunkName: "ChatStudio" */ "@/views/ChatStudio/index"),
+  //       meta: {
+  //         title: "聊天工作室",
+  //         icon: "ForkSpoon",
+  //         keep: true
+  //       }
+  //     },
+  //   ]
+  // },
 ];
 
 export default routes;
