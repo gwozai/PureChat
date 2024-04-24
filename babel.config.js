de03b6e7d1e0b610411ef813219d4c0c7803651e
yongkang <2607881950@qq.com>
2024-04-24 13:32:29 +0800

    依赖升级

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 // 主要用于在当前和旧的浏览器或环境中，将 ECMAScript 2015 + 代码转换为 JavaScript 向后兼容版本的代码
 const plugins = [];
 if (process.env.NODE_ENV === "production") {
-  // plugins.push(["transform-remove-console", { exclude: ["error", "warn"] }]);
+  plugins.push(["transform-remove-console", { exclude: ["error", "warn"] }]);
 }
 module.exports = {
   presets: ["@vue/cli-plugin-babel/preset"],
