b9737ff6b699652b56a47dc775a9179b4854b58e
yongkang <2607881950@qq.com>
2024-05-07 12:56:51 +0800

    refactor: cardGrid && listGrid

--- src/views/chatStudio/AddressBook/index.vue
+++ src/views/chatStudio/AddressBook/index.vue
@@ -1,59 +1,47 @@
 <template>
   <div class="flex">
     <div class="aside">
-      <div
-        class="list"
-        v-for="item in data"
-        :key="item.icon"
-        @click="onClick(item)"
-        :class="{
-          selectd: active === item.icon,
-        }"
-      >
-        <div class="icon">
-          <FontIcon :iconName="item.icon" />
-        </div>
-        <div class="title">{{ item.title }}</div>
-      </div>
+      <ListGrid @onActive="onActive" />
     </div>
     <div class="main">
-      <div v-if="active === 'ForkSpoon'">
-        <div class="items-box" v-for="item in friend" :key="item.userID">
-          <div class="left-item">
-            <UserAvatar words="3" shape="circle" :nickName="item.nick" :url="item.avatar" />
-          </div>
-          <div class="right-item">
-            <p>{{ item.nick }}</p>
-            <el-icon @click="onFriend(item.userID)"><Position /></el-icon>
-          </div>
-        </div>
-      </div>
+      <el-scrollbar class="h-full">
+        <CardGrid v-if="active === 'ForkSpoon'" type="C2C" :item="friend" />
+        <CardGrid v-else-if="active === 'IceCreamRound'" type="GROUP" :item="groupList" />
+        <!-- <CardGrid v-else-if="active === 'CollectionTag'" type="GROUP" :item="groupListInfo" /> -->
+      </el-scrollbar>
     </div>
   </div>
 </template>
 
 <script>
+import CardGrid from "./CardGrid";
+import ListGrid from "./ListGrid";
+import { restApi } from "@/api/node-admin-api/index";
 import { getUserProfile } from "@/api/im-sdk-api/index";
+import { mapState } from "vuex";
+
 export default {
   name: "AddressBook",
   data() {
     return {
-      data: [
-        {
-          title: "我的好友",
-          icon: "ForkSpoon",
-        },
-        // {
-        //   title: "我的群聊",
-        //   icon: "IceCreamRound",
-        // },
-      ],
       active: "ForkSpoon",
       friend: [],
+      groupListInfo: [],
     };
   },
-  mounted() {
+  components: {
+    ListGrid,
+    CardGrid,
+  },
+  computed: {
+    ...mapState({
+      groupList: (state) => state.groupinfo.groupList,
+    }),
+  },
+  async mounted() {
     this.init();
+    // this.getGroupList();
+    this.$store.dispatch("getGroupList");
   },
   methods: {
     async init() {
@@ -62,16 +50,29 @@ export default {
       const { code, data } = await getUserProfile(list);
       this.friend = data;
     },
-    onFriend(id) {
+    async getGroupList() {
+      const { GroupIdList } = await restApi({
+        funName: "getAppidGroupList",
+      });
+      const result = GroupIdList.map((t) => t.GroupId);
+      const { GroupInfo } = await restApi({
+        params: result,
+        funName: "getGroupInfo",
+      });
+      console.log(GroupInfo);
+      this.groupListInfo = GroupInfo;
+    },
+    onFriend({ id, type = "C2C" }) {
+      // "GROUP" : "C2C";
       this.$store.commit("TAGGLE_OUE_SIDE", "message");
-      this.$store.dispatch("CHEC_OUT_CONVERSATION", { convId: `C2C${id}` });
+      this.$store.dispatch("CHEC_OUT_CONVERSATION", { convId: `${type}${id}` });
       setTimeout(() => {
         const dom = document.getElementById(`message_C2C${id}`);
         if (!dom) return;
         dom.scrollIntoView({ behavior: "smooth", block: "center" });
       }, 300);
     },
-    onClick({ icon }) {
+    onActive(icon) {
       this.active = icon;
     },
   },
@@ -85,60 +86,5 @@ export default {
   height: 100%;
   padding: 3px 8px 8px;
   border-right: 1px solid #00000017;
-  .list {
-    display: flex;
-    align-items: center;
-    border-radius: 5px;
-    cursor: pointer;
-  }
-  .icon {
-    height: 35px;
-    width: 30px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-  .title {
-    padding-left: 5px;
-  }
-}
-.selectd {
-  background-color: #f5f5f5;
-  color: rgb(114, 184, 249);
-  .el-icon {
-    color: rgb(114, 184, 249);
-  }
-}
-
-.items-box {
-  display: flex;
-  width: 218px;
-  min-height: 50px;
-  border: 1px solid #eeeaea;
-  border-radius: 10px;
-  padding: 10px;
-  box-sizing: border-box;
-  .left-item {
-    img {
-      width: 40px;
-      height: 40px;
-    }
-  }
-  .right-item {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 150px;
-    padding-left: 10px;
-    .el-icon {
-      cursor: pointer;
-    }
-  }
-}
-.main {
-  & > div {
-    display: flex;
-    flex-wrap: wrap;
-  }
 }
 </style>
